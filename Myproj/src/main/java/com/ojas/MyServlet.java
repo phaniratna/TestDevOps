7097dde7560ef30644526da2e892ab5a34b8cbff
phaniratna <phani.ratna@gmail.com>
2022-10-21 12:14:12 +0530

    First time commiting the code

---
+++ Myproj/src/main/java/com/ojas/MyServlet.java
@@ -0,0 +1,35 @@
+package com.ojas;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MyServlet
+ */
+public class MyServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MyServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter pw = response.getWriter();
+		
+	   pw.println("<h1 font='red'> welcome to " + request.getParameter("uname"));
+	}
+
+}
---
+++ Myproj/src/main/webapp/index.jsp
@@ -0,0 +1,10 @@
+<html>
+<body>
+<form action="MyServlet">
+<h2>Hello World!</h2>
+  Enter Name:<input type=text name ="uname" />
+  <input type=submit value="Go">
+  
+</form>
+</body>
+</html>
